ae49a24506771161b26d4d0f75e7288f80d5650a
Miguel William <116318328+miguelwilliam@users.noreply.github.com>
2023-07-13 22:34:19 -0300

    Classes criadas

--- Game.py
+++ Game.py
@@ -1,28 +1,35 @@
 #import pygame
 from GameObjects import *
 
-#tabuleiro 10x10
-#ordem das nested lists: [linha][coluna](essa ultima é formada por listas dentro, porque pode ter mais de um animal na mesma casa)
-tabuleiro = [
-            [[], [], [], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], [], [], []]
-            ]
 
-for linha in tabuleiro:
-    linha_atual = ''
-    for coluna in linha:
-        if len(coluna) == 0:
-            linha_atual+='X'
-        else:
-            linha_atual+=str(len(coluna))
-        linha_atual+=' '
-    print(linha_atual)
-print(len(tabuleiro))
+class Game:
+    def __init__(self):
+        self.turno = 1
+        #tabuleiro 10x10
+        #ordem das nested lists: [linha][coluna](essa ultima é formada por listas dentro, porque pode ter mais de um animal na mesma casa)
+        self.tabuleiro =  [
+            [['oooooo'], [], [], [], [], [], [], [], [], []],
+            [[], [], [], [], [], [], [], [], [], []],
+            [[], [], [], [], [], [], [], [], [], []],
+            [[], [], [], [], [], [], [], [], [], []],
+            [[], [], [], [], [], [], [], [], [], []],
+            [[], [], [], [], [], [], [], [], [], []],
+            [[], [], [], [], [], [], [], [], [], []],
+            [[], [], [], [], [], [], [], [], [], []],
+            [[], [], [], [], [], [], [], [], [], []],
+            [[], [], [], [], [], [], [], [], [], ['000000']]
+            ]
+    
+    def debug(self):
+        for linha in self.tabuleiro:
+            linha_atual = ''
+            for coluna in linha:
+                if len(coluna) == 0:
+                    linha_atual+='X'
+                else:
+                    linha_atual+=str(len(coluna))
+                linha_atual+=' '
+            print(linha_atual)
+
+game = Game()
+game.debug()
--- GameObjects.py
+++ GameObjects.py
@@ -14,7 +14,7 @@ class Animal():
         self.direcaoY = 'cima' # cima / baixo
 
     def andar(self):
-        if self.turno % 2 == 0:
+        if self.game.turno % 2 == 0:
             if self.direcaoX == 'direita':
                 if self.pos[0] + self.vel > len(self.game.tabuleiro):
                     self.pos[0] = len(self.game.tabuleiro)
@@ -41,7 +41,7 @@ class Animal():
                     self.direcaoY = 'cima'
                 else:
                     self.pos[1] -= self.vel
-        self.turno += 1
+        self.game.turno += 1
     
     def checar_colisao(self, nome_colisao):
         if self.name and nome_colisao in self.game.tabuleiro[self.pos[1]-1][self.pos[0]-1]:
@@ -59,3 +59,124 @@ class Animal():
 = posiçao X: {self.pos[0]}
 = posiçao Y: {self.pos[1]}
               ''')
+        
+    def getPos(self):
+        return self.pos
+    def setPos(self, newX, newY):
+        self.pos = [newX, newY]
+    
+    def getNome(self):
+        return self.nome
+    def setNome(self, newNome):
+        self.nome = newNome
+    
+    def getCor(self):
+        return self.cor
+    def setCor(self, newCor):
+        self.cor = newCor
+
+    def getSexo(self):
+        return self.sexo
+    def setSexo(self, newSexo):
+        self.sexo = newSexo
+
+    def getVel(self):
+        return self.vel
+    def setVel(self, newVel):
+        self.vel = newVel
+
+    def getPeso(self):
+        return self.peso
+    def setPeso(self, newPeso):
+        self.peso = newPeso
+
+    def getStamina(self):
+        return self.stamina
+    def setStamina(self, newStamina):
+        self.stamina = newStamina
+
+
+
+class Leao(Animal):
+    def __init__(self, nome, cor, sexo, velocidade, peso, stamina, posX, posY, game, idade):
+        super().__init__(nome, cor, sexo, velocidade, peso, stamina, posX, posY, game)
+        self.idade = idade
+    
+    def rugir(self):
+        print('rugido raarwwwww')
+    
+    def getIdade(self):
+        return self.idade
+    def setIdade(self, newIdade):
+        self.idade = newIdade
+
+class Cachorro(Animal):
+    def __init__(self, nome, cor, sexo, velocidade, peso, stamina, posX, posY, game, idade, raca):
+        super().__init__(nome, cor, sexo, velocidade, peso, stamina, posX, posY, game)
+        self.idade = idade
+        self.raca = raca
+    
+    def latir(self):
+        print('latido auauauauau')
+    
+    def getIdade(self):
+        return self.idade
+    def setIdade(self, newIdade):
+        self.idade = newIdade
+
+    def getRaca(self):
+        return self.raca
+    def setRaca(self, newRaca):
+        self.raca = newRaca
+
+class Gato(Animal):
+    def __init__(self, nome, cor, sexo, velocidade, peso, stamina, posX, posY, game, raca):
+        super().__init__(nome, cor, sexo, velocidade, peso, stamina, posX, posY, game)
+        self.raca = raca
+    
+    def miar(self):
+        print('miado miauuuuuuuuu')
+    
+    def getRaca(self):
+        return self.raca
+    def setRaca(self, newRaca):
+        self.raca = newRaca
+
+class Vaca(Animal):
+    def __init__(self, nome, cor, sexo, velocidade, peso, stamina, posX, posY, game, raca):
+        super().__init__(nome, cor, sexo, velocidade, peso, stamina, posX, posY, game)
+        self.raca = raca
+    
+    def mugir(self):
+        print('mugido muuuuuuuuuuuu')
+    
+    def getRaca(self):
+        return self.raca
+    def setRaca(self, newRaca):
+        self.raca = newRaca
+
+class Ovelha(Animal):
+    def __init__(self, nome, cor, sexo, velocidade, peso, stamina, posX, posY, game, idade):
+        super().__init__(nome, cor, sexo, velocidade, peso, stamina, posX, posY, game)
+        self.idade = idade
+    
+    def balir(self):
+        print('balindo meeeeeeeeehhh')
+
+    def getIdade(self):
+        return self.idade
+    def setIdade(self, newIdade):
+        self.idade = newIdade
+
+class Pulga(Animal):
+    def __init__(self, nome, cor, sexo, velocidade, peso, stamina, posX, posY, game, idade):
+        super().__init__(nome, cor, sexo, velocidade, peso, stamina, posX, posY, game)
+        self.idade = idade
+    
+    def sugar(self):
+        print('sugando glubglubglubglub')
+
+    def getIdade(self):
+        return self.idade
+    def setIdade(self, newIdade):
+        self.idade = newIdade
